d897c515b7313d12256699e4bdcc9d143fc2d85b
Christian Ost <hallo@christianost.de>
2021-07-25 19:07:02 +0200

    fetch a single book the local example server via http

---
+++ src/components/Book.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import { Book as BookI } from "../domain/types";
+
+const Book: React.FC = () => {
+  const [book, setBook] = useState<BookI | null>(null);
+
+  useEffect(() => {
+    async function fetchBook() {
+      const response = await fetch("http://localhost:4730/books/9781783983667");
+      const book = await response.json();
+      setBook(book);
+    }
+    fetchBook();
+  }, []);
+
+  return (
+    <div>
+      <p>{book?.title}</p>
+      <p>{book?.subtitle}</p>
+      <p>{book?.numPages} pages</p>
+    </div>
+  );
+};
+
+export default Book;
