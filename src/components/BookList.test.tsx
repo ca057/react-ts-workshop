2238b0454567b003c30e2874991b334bc8a0cfa3
Christian Ost <hallo@christianost.de>
2021-07-26 18:16:46 +0200

    add basic tests for single book, wrap all tests in describe-blocks

--- src/components/BookList.test.tsx
+++ src/components/BookList.test.tsx
@@ -3,16 +3,18 @@ import { render } from "@testing-library/react";
 
 import BookList from "./BookList";
 
-test("renders all books", () => {
-  const books = [
-    { title: "My first book", price: 11.11 },
-    { title: "My second book", price: 22.22 },
-  ];
-  const { getByText } = render(
-    <BookList books={books} onItemClick={jest.fn()} />
-  );
+describe("components/BookList", () => {
+  test("renders all books", () => {
+    const books = [
+      { title: "My first book", price: 11.11 },
+      { title: "My second book", price: 22.22 },
+    ];
+    const { getByText } = render(
+      <BookList books={books} onItemClick={jest.fn()} />
+    );
 
-  for (const book of books) {
-    expect(getByText(book.title)).toBeTruthy();
-  }
+    for (const book of books) {
+      expect(getByText(book.title)).toBeTruthy();
+    }
+  });
 });
--- src/components/Counter.test.tsx
+++ src/components/Counter.test.tsx
@@ -2,26 +2,28 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import Counter from "./Counter";
 
-test("should be possible to increment the counter", () => {
-  const initialValue = 0;
-  const { getByText } = render(<Counter initialValue={initialValue} />);
+describe("components/Counter", () => {
+  test("should be possible to increment the counter", () => {
+    const initialValue = 0;
+    const { getByText } = render(<Counter initialValue={initialValue} />);
 
-  const countElement = getByText(initialValue);
-  const incrementButton = getByText("+");
+    const countElement = getByText(initialValue);
+    const incrementButton = getByText("+");
 
-  fireEvent.click(incrementButton);
+    fireEvent.click(incrementButton);
 
-  expect(countElement.textContent).toBe("1");
-});
+    expect(countElement.textContent).toBe("1");
+  });
 
-test("should be possible to decrement the counter", () => {
-  const initialValue = 0;
-  const { getByText } = render(<Counter initialValue={initialValue} />);
+  test("should be possible to decrement the counter", () => {
+    const initialValue = 0;
+    const { getByText } = render(<Counter initialValue={initialValue} />);
 
-  const countElement = getByText(initialValue);
-  const decrementButton = getByText("-");
+    const countElement = getByText(initialValue);
+    const decrementButton = getByText("-");
 
-  fireEvent.click(decrementButton);
+    fireEvent.click(decrementButton);
 
-  expect(countElement.textContent).toBe("-1");
+    expect(countElement.textContent).toBe("-1");
+  });
 });
