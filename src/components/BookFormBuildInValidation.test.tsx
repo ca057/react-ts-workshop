d6ffd92b993b616ffb879c6505f50fbbf958b490
Christian Ost <hallo@christianost.de>
2021-07-27 08:36:54 +0200

    add tests for book form with build in validation

---
+++ src/components/BookFormBuildInValidation.test.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { fireEvent, render } from "@testing-library/react";
+
+import BookFormBuildInValidation from "./BookFormBuildInValidation";
+
+describe("components/BookFormBuildInValidation", () => {
+  test("renders the passed book", () => {
+    const book = { title: "Java Web Scraping Handbook", isbn: "1001606140805" };
+    const { getByDisplayValue } = render(
+      <BookFormBuildInValidation book={book} onSubmit={jest.fn()} />
+    );
+
+    expect(getByDisplayValue(book.title)).toBeTruthy();
+    expect(getByDisplayValue(book.isbn)).toBeTruthy();
+  });
+
+  test("allows editing data of a book", () => {
+    const book = { title: "Java Web Scraping Handbook", isbn: "1001606140805" };
+    const { getByDisplayValue } = render(
+      <BookFormBuildInValidation book={book} onSubmit={jest.fn()} />
+    );
+
+    const updatedBookTitle = "Java Web Scraping Handbook Next Volume";
+    fireEvent.change(getByDisplayValue(book.title), {
+      target: { value: updatedBookTitle },
+    });
+
+    expect(() => getByDisplayValue(book.title)).toThrow();
+    expect(getByDisplayValue(updatedBookTitle)).toBeTruthy();
+    expect(getByDisplayValue(book.isbn)).toBeTruthy();
+  });
+
+  test("calls callback with entered data when form is submitted", () => {
+    const book = { title: "Java Web Scraping Handbook", isbn: "1001606140805" };
+    const onSubmitMock = jest.fn();
+    const { getByText } = render(
+      <BookFormBuildInValidation book={book} onSubmit={onSubmitMock} />
+    );
+
+    fireEvent.click(getByText("Submit"));
+
+    expect(onSubmitMock).toHaveBeenCalledWith(book);
+  });
+});
