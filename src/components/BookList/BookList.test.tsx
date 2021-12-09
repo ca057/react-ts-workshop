25f9d1b4a25ab04a2d5a0abbcf72c3a3f02845e2
André Kovac <me@andrekovac.com>
2021-12-10 00:35:21 +0100

    Add snapshot tests for BookList and BookListItem

--- src/components/BookList/BookList.test.tsx
+++ src/components/BookList/BookList.test.tsx
@@ -1,14 +1,27 @@
 import { render } from "@testing-library/react";
+import renderer from "react-test-renderer";
 
 import BookList from ".";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, MemoryRouter } from "react-router-dom";
 
 describe("components/BookList", () => {
+  const books = [
+    { title: "My first book", price: 11.11 },
+    { title: "My second book", price: 22.22 },
+  ];
+
+  test(`renders a booklist correctly`, () => {
+    const tree = renderer
+      .create(
+        <MemoryRouter>
+          <BookList books={books} />
+        </MemoryRouter>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
   test("renders all books", () => {
-    const books = [
-      { title: "My first book", price: 11.11 },
-      { title: "My second book", price: 22.22 },
-    ];
     const { getByText } = render(<BookList books={books} />, {
       wrapper: BrowserRouter,
     });
--- src/components/BookListItem/BookListItem.test.tsx
+++ src/components/BookListItem/BookListItem.test.tsx
@@ -1,11 +1,24 @@
 import { render } from "@testing-library/react";
+import renderer from "react-test-renderer";
 
 import BookListItem from ".";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, MemoryRouter } from "react-router-dom";
 
 describe("components/BookListItem", () => {
+  const book = { title: "My first book", price: 11.11 };
+
+  test(`renders a booklist item correctly`, () => {
+    const tree = renderer
+      .create(
+        <MemoryRouter>
+          <BookListItem book={book} />
+        </MemoryRouter>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
   test("renders book with title", () => {
-    const book = { title: "My first book", price: 11.11 };
     const { getByText } = render(<BookListItem book={book} />, {
       // BookListItem uses <Link /> component from react-router-dom
       wrapper: BrowserRouter,
