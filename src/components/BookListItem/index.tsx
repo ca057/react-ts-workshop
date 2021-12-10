bd2a7b6a736b58ed69ae74bb2fd102890dc4956a
André Kovac <me@andrekovac.com>
2021-12-10 15:27:20 +0100

    Update BookListItem and changed snapshot tests

--- src/components/BookListItem/index.tsx
+++ src/components/BookListItem/index.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import { Book } from "../../domain/types";
+import "./styles.css";
 
 interface BookListItemProps {
   book: Book;
 }
 
 const BookListItem: React.VFC<BookListItemProps> = ({ book }) => (
-  <Link to={`/books/${book.isbn}`}>{book.title}</Link>
+  <Link to={`/books/${book.isbn}`}>
+    <div className="booklist__item">{book.title}</div>
+  </Link>
 );
 
 export default BookListItem;
