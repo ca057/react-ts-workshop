9abea00310c92ef3bdb81d55845ff15fbf1847ac
Christian Ost <hallo@christianost.de>
2022-04-18 10:46:00 +0200

    feat: migrate to React 18 starting code

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const container = document.getElementById('root')
+if (!container) throw new Error("container element not in DOM")
+
+const root = ReactDOM.createRoot(container)
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
