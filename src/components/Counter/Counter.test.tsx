2b655093ad961d6a04177f2c5dea76b8deab8f9b
Christian Ost <hallo@christianost.de>
2022-04-18 21:59:39 +0200

    test: add user-event utility, re-write tests using user-event

--- src/components/Counter/Counter.test.tsx
+++ src/components/Counter/Counter.test.tsx
@@ -1,34 +1,41 @@
 // Use custom render method (so that the test also works with Redux)
 import { render, fireEvent, screen } from "../../common/util/test-utils";
+import userEvent from "@testing-library/user-event";
 import Counter from ".";
 
 describe("components/Counter", () => {
-  test("should be possible to reset the counter", () => {
+  test("should be possible to reset the counter", async () => {
+    const user = userEvent.setup();
+
     render(<Counter />);
     const resetButton = screen.getByText("reset");
-    fireEvent.click(resetButton);
+    await user.click(resetButton);
 
     expect(screen.getByText("0")).toBeTruthy();
   });
 
-  test("should be possible to increment the counter", () => {
+  test("should be possible to increment the counter", async () => {
+    const user = userEvent.setup();
+
     render(<Counter />);
     // reset initial value to 0
-    fireEvent.click(screen.getByText("reset"));
+    await user.click(screen.getByText("reset"));
 
     const incrementButton = screen.getByText("+");
-    fireEvent.click(incrementButton);
+    await user.click(incrementButton);
 
     expect(screen.getByText("1")).toBeTruthy();
   });
 
-  test("should be possible to decrement the counter", () => {
+  test("should be possible to decrement the counter", async () => {
+    const user = userEvent.setup();
+
     render(<Counter />);
     // reset initial value to 0
-    fireEvent.click(screen.getByText("reset"));
+    await user.click(screen.getByText("reset"));
 
     const decrementButton = screen.getByText("-");
-    fireEvent.click(decrementButton);
+    await user.click(decrementButton);
 
     expect(screen.getByText("-1")).toBeTruthy();
   });
